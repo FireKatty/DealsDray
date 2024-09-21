04e995c40ce2de8594110c7ae00116fa38f8449c
Abhishek Katiyar <96065697+FireKatty@users.noreply.github.com>
2024-09-21 07:21:49 +0530

    Update connectDatabase.js

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectToDatabase = async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/Firekatty', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect('mongodb://localhost:27017/Dealsdray', { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Database Connected")
     } catch (error) {
         console.log("Error")
